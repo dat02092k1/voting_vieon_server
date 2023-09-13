3cb89837a164e1e1766c535528c4dad66a486fb0
dat02092k1 <ngodat02092001@gmail.com>
2023-09-13 18:14:52 +0700

    config cors

--- src/app.js
+++ src/app.js
@@ -11,6 +11,18 @@ app.use(express.urlencoded({
     extended: true
 }));
 
+// config cors
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+app.configure(function() {
+    app.use(allowCrossDomain);
+    //some other code
+});
+
 // init db
 require('./database/init.mongodb');
 
