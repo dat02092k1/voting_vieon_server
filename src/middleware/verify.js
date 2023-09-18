418a089d487e40c941a5602923a14d3f81208280
dat02092k1 <ngodat02092001@gmail.com>
2023-09-18 11:48:35 +0700

    authorization with admin

--- src/middleware/verify.js
+++ src/middleware/verify.js
@@ -5,46 +5,57 @@ const {asyncHandler} = require("./auth");
 require('dotenv').config();
 
 class verifyMiddileware {
-     verifyToken = (req, res, next) => {
+    verifyToken = (req, res, next) => {
         const token = req.headers[utilContainers.HEADER.AUTHORIZATION];
         if (token) {
             const accessToken = token.split(" ")[1];
-            console.log(accessToken);
+
             jwt.verify(accessToken, process.env.JWT_SECRET, (err, user) => {
-              if (err) {
-                  console.log(err);
-                throw new Api403Error("Forbidden request");
-              }
-              req.user = user;
-              next();
+                if (err) {
+                    console.log(err);
+                    throw new Api403Error("Forbidden request");
+                }
+                req.user = user;
+                next();
             });
-          } else {
+        } else {
             throw new Api401Error("You're not authenticated");
-          }
+        }
     }
 
-     authentication = asyncHandler(async (req, res, next) => {
+    authentication = asyncHandler(async (req, res, next) => {
         /**
          * 1 - check api key???
          * 2 - get access token
          * 3 - verify Token
          * 4 - check user exists ?
-         * 5 - check keyStore with this userId
-         * 6 - OK all => return next()
+         * 5 - OK all => return next()
          */
             // 1
         const apiKey = req.headers[utilContainers.HEADER.API_KEY];
-         console.log(apiKey)
+
         if (!apiKey) throw new Api403Error('Forbidden request');
 
         if (apiKey !== process.env.API_KEY) throw new Api403Error('Forbidden request');
 
-         this.verifyToken(req, res, () => {
-             next();
-         })
-
+        this.verifyToken(req, res, () => {
+            next();
+        })
     })
 
+    isAdmin = asyncHandler(async (req, res, next) => {
+        const apiKey = req.headers[utilContainers.HEADER.API_KEY];
+
+        if (!apiKey) throw new Api403Error('Forbidden request');
+
+        if (apiKey !== process.env.API_KEY) throw new Api403Error('Forbidden request');
+
+        this.verifyToken(req, res, () => {
+            if (req.user.role !== utilContainers.roleUsers[1]) throw new Api403Error('Only admin can do this');
+            next();
+        })
+    })
 }
+
 
 module.exports = new verifyMiddileware();
--- src/routes/contestant/contestant.js
+++ src/routes/contestant/contestant.js
@@ -3,11 +3,11 @@ const router = express.Router()
 const contestantController = require('../../controllers/contestant.controller')
 const verifyMiddleware = require('../../middleware/verify')
 
-router.post('/contestant', verifyMiddleware.verifyToken, contestantController.create)
+router.post('/contestant', verifyMiddleware.isAdmin, contestantController.create)
 router.get('/contestant', contestantController.getAll)
 router.get('/contestant/:id', contestantController.getById)
-router.delete('/contestant', contestantController.delete)
-router.put('/contestant', contestantController.update)
+router.delete('/contestant', verifyMiddleware.isAdmin, contestantController.delete)
+router.put('/contestant', verifyMiddleware.isAdmin, contestantController.update)
 
 
 module.exports = router;
