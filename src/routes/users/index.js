f81eb47893d973f5f975a51728bf6eb9d6fd2605
dat02092k1 <ngodat02092001@gmail.com>
2023-09-22 15:20:55 +0700

    fix route url post

--- src/routes/users/index.js
+++ src/routes/users/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../../controllers/user.controller')
 
-router.post('/user/create', userController.create)
+router.post('/user', userController.create)
 router.get('/user', userController.getAll)
 router.get('/user/:id', userController.getById)
 router.put('/user/:id', userController.update)
